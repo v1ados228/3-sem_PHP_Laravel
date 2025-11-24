e0b1dea2ed1306263e717cdfa657eb47837b4840
Vlad Morozov <vladmorozov06@gmail.com>
2025-11-25 00:15:37 +0300

    lab2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function (){
-    return view('layout');
-});
+Route::get('/',[MainController::class, 'index']);
+Route::get('/full_image/{img}',[MainController::class, 'show']);
 
 Route::get('/about', function(){
     return view('main.about');
